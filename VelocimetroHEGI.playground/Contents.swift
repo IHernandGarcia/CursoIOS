37990bc804ed292012178388573ac016638f254b
IHernandGarcia <iherndgarcia@hotmail.com>
2016-05-04 22:01:12 -0500

    VelocimentroFinalHegi
    TareaVelocimetro

--- VelocimetroHEGI.playground/Contents.swift
+++ VelocimetroHEGI.playground/Contents.swift
@@ -2,27 +2,56 @@
 
 import UIKit
 
-enum Velocidades:Int {
-    case Apagado = 0,
-    VelocidadBaja = 20,
-    VelocidadMedia = 50,
-    VelocidadAlta = 120
+enum Velocidades: Int {
+    case Apagado=0,
+    VelocidadBaja=20,
+    VelocidadMedia=50,
+    VelocidadAlta=120
     
-    init( velocidadInicial : Velocidades ){
-        self=velocidadInicial
+    init(velocidadInicial: Velocidades) {
+        self = velocidadInicial
     }
-    
-
 }
+
 class Auto {
-    var velocidad : Velocidades
-    init (velocidad : Velocidades){
-        self.velocidad = .Apagado
+    var velocidad: Velocidades
+    
+    init(){
+        self.velocidad = Velocidades(velocidadInicial: .Apagado)
     }
     
-    func cambioDeVelocidad () -> ( actual : Int, velocidadEnCadena: String)
-    {
+    func CambioDeVelocidad() ->(actual:Int, VelocidadEnCadena:String){
+        switch self.velocidad{
+        case .Apagado:
+            let resultado = (self.velocidad.rawValue, "Apagado")
+            self.velocidad = .VelocidadBaja
+            return resultado
+            
+        case .VelocidadBaja:
+            let resultado = (self.velocidad.rawValue, "Velocidad Baja")
+            self.velocidad = .VelocidadMedia
+            return resultado
+            
+        case .VelocidadMedia:
+            let resultado = (self.velocidad.rawValue, "Velocidad Media")
+            self.velocidad = .VelocidadAlta
+            return resultado
+            
+        case .VelocidadAlta:
+            let resultado = (self.velocidad.rawValue, "Velocidad Alta")
+            self.velocidad = .VelocidadMedia
+            return resultado
+            
+        }
         
-        return (0,"velocidad baja")
-    }// Func
-}// Class
+    }
+}
+
+var auto = Auto()
+
+for i in 1...20{
+    
+    let salidaVelocidad = auto.CambioDeVelocidad()
+    print("\(salidaVelocidad.actual), \(salidaVelocidad.VelocidadEnCadena)\n")
+    
+}
